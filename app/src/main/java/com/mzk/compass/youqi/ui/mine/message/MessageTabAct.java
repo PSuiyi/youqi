bd34de0f053991ae105815a36d2f07063f9f4f4e
panqiming <497555328@qq.com>
2018-04-01 00:23:12 +0800

    +优企代码上传

---
+++ app/src/main/java/com/mzk/compass/youqi/ui/mine/message/MessageTabAct.java
@@ -0,0 +1,36 @@
+package com.mzk.compass.youqi.ui.mine.message;
+
+import com.mzk.compass.youqi.R;
+import com.mzk.compass.youqi.base.BaseAppActivity;
+
+/**
+ * Date： 2018/4/1 2018
+ * User： PSuiyi
+ * Description：
+ */
+public class MessageTabAct extends BaseAppActivity {
+    @Override
+    protected int[] getLayoutResource() {
+        return new int[]{R.layout.common_tab_layout, 1};
+    }
+
+    @Override
+    protected void initializeVariate() {
+
+    }
+
+    @Override
+    protected void initializeNavigation() {
+
+    }
+
+    @Override
+    protected void initializeView() {
+
+    }
+
+    @Override
+    protected void loadDataFromServer() {
+
+    }
+}
---
+++ app/src/main/java/com/mzk/compass/youqi/ui/mine/vip/VipCenterAct.java
@@ -0,0 +1,35 @@
+package com.mzk.compass.youqi.ui.mine.vip;
+
+import com.mzk.compass.youqi.base.BaseAppActivity;
+
+/**
+ * Date： 2018/4/1 2018
+ * User： PSuiyi
+ * Description：
+ */
+public class VipCenterAct extends BaseAppActivity {
+    @Override
+    protected int[] getLayoutResource() {
+        return new int[0];
+    }
+
+    @Override
+    protected void initializeVariate() {
+
+    }
+
+    @Override
+    protected void initializeNavigation() {
+
+    }
+
+    @Override
+    protected void initializeView() {
+
+    }
+
+    @Override
+    protected void loadDataFromServer() {
+
+    }
+}
